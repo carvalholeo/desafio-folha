f0ca1d4c842e75240a4a4d9c293405c861c00805
Leonardo Carvalho <carvalho.csleo@gmail.com>
2019-11-08 00:02:23 -0300

    Implement the logic to calculate engineers quantity

--- app/Http/Controllers/StarSystemController.php
+++ app/Http/Controllers/StarSystemController.php
@@ -6,5 +6,94 @@ use Illuminate\Http\Request;
 
 class StarSystemController extends Controller
 {
-    //
+    private $star;
+    private $customStar;
+    private $engineers;
+    private $counterDefaultStar;
+    private $counterEnginners;
+    private $counterCustomStar;
+    private $starHasVowels;
+
+    private function calculateEngineers($star)
+    {
+        $counter = 1;
+        for ($i = 0; $i < strlen($star); $i++) {
+            if ($star[$i] == 'A')
+                $counter *= 1;
+            
+            if ($star[$i] == 'E')
+                $counter *= 2;
+            
+            if ($star[$i] == 'I')
+                $counter *= 3;
+
+            if ($star[$i] == 'O')
+                $counter *= 5;
+            
+            if ($star[$i] == 'U')
+                $counter *= 8;
+        }
+        return $counter;
+    }
+
+    private function verifyIfExistsVowel($star)
+    {
+        $verifyStar = str_split($star);
+
+        foreach ($verifyStar as $singleLetter) {
+            if ($singleLetter == 'A' ||
+                $singleLetter == 'E' ||
+                $singleLetter == 'I' ||
+                $singleLetter == 'O' ||
+                $singleLetter == 'U')
+            {
+                return TRUE;
+            } else {
+                continue;
+            }
+        }
+        return FALSE;
+    }
+
+    public function show()
+    {
+        return view('index');
+    }
+
+    private function customStar($request)
+    {
+        $customStar = $request->input('customStar');
+        $engineers = (int) $request->input('engineers');
+
+        $customStar = strtoupper($customStar);
+
+        $starHasVowels = self::verifyIfExistsVowel($customStar);
+        
+        if ($starHasVowels){
+            $counterCustomStar = self::calculateEngineers($customStar);
+        } else {
+            $counterCustomStar = 0; 
+        }
+
+        if($engineers == $counterCustomStar) {
+            return $request->session()
+                    ->flash('customSuccess', "VOCÊ ACERTOU!! A quantidade de engenheiros para a estrela $customStar é $counterCustomStar");
+        } else {
+            return $request->session()
+                    ->flash('customWrong', "Infelizmente, você errou. A quantidade de engenheiros para a estrela $customStar é $counterCustomStar");
+        }
+    }
+
+    public function form(Request $request)
+    {
+        $star = $request->input('star');
+        $counterDefaultStar = self::calculateEngineers($star);
+        $request->session()->flash('default', "A quantidade de engenheiros para a estrela $star é $counterDefaultStar");
+
+        if ($request->filled('customStar'))
+            {
+                self::customStar($request);
+            }
+            return redirect('/')->withInput();
+    }
 }
