5eee8f7b87d03bc1278431ff77712c96ca2cd704
Leonardo Carvalho <carvalho.csleo@gmail.com>
2019-11-07 23:54:27 -0300

    Add a new type of flash message

--- resources/views/components/flash-messages.blade.php
+++ resources/views/components/flash-messages.blade.php
@@ -16,8 +16,15 @@
             </div>
 @endif
 
-@if ($message = Session::get('custom'))
+@if ($message = Session::get('customSuccess'))
             <div class="alert alert-info alert-block">
+                <button type="button" class="close" data-dismiss="alert">×</button>	
+                    {{ $message }}
+            </div>
+@endif
+
+@if ($message = Session::get('customWrong'))
+            <div class="alert alert-danger alert-block">
                 <button type="button" class="close" data-dismiss="alert">×</button>	
                     {{ $message }}
             </div>
